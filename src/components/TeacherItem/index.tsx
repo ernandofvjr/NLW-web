22d9bc6180682427098e5dd778f414847147769b
Ernando <ernandofvjr@hotmail.com>
2020-08-05 17:14:19 -0300

    Adiciona teacher form page
    -Cria conexão com a api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    subject: string;
+    cost: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id
+        });
+    }
+
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/24616057?s=460&u=4adbcebbd0b7eba992f7ecbc544edfaba0545f57&v=4" alt="Imagem"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Ernando Ferreira</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae imperdiet sem, vel cursus sem.
-                <br/>
-                <br/>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ex leo, sollicitudin posuere ullamcorper a, vestibulum eget lorem. Pellentesque fringilla enim felis, eu pharetra enim sodales vitae. Pellentesque ullamcorper, nisl ac porttitor blandit, lacus turpis maximus odio, vel auctor est orci et diam. Donec dignissim eleifend tortor nec tincidunt. Ut tristique dictum nulla, id finibus lacus maximus laoreet. Nam at dapibus justo, quis scelerisque mi. Integer at semper purus, vitae cursus dui.
-            </p>
+
+            <p> {teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 20,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
 
         </article>
